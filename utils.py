63111eddc11b3b8127a56149041653c530161b7c
Nathan Solomon <nathansolomon1678@gmail.com>
2020-08-20 10:55:26 -0700

    Rename stuff.py to utils.py and add more messy code

--- utils.py
+++ utils.py
@@ -1,29 +1,52 @@
 """
 A collection of functions to use in analyzing the SARS-CoV2 spike protein
+
+Throughout this file, groups of points will be stored mostly as matrices with
+each row representing a point, and each column representing a dimension (x, y, or z)
 """
 import math
 import numpy as np
 from typing import *
 
-def backbone(protein_name: str, chain: chr, only_alpha_carbons=False) -> np.matrix[float]]:
+def missing_residues(protein_name: str, *chains: chr) -> List[int]:
+    # Returns the indeces of residues that are in some chains but not all
+    with open(f'{protein_name.lower()}.pdb') as pdb_file:
+        pdb_data = pdb_file.read()
+    lines = [line for line in pdb_data.split('\n') if line[0:4] == 'ATOM' and line[21] in chains]
+    missing_residues = []
+    residues_for_each_chain = {}
+    for line in lines:
+        residue_num = int(line[22:26])
+        chain = line[21]
+        residues_for_each_chain[chain] = residues_for_each_chain.get(chain, set())
+        residues_for_each_chain[chain].add(residue_num)
+    sets_of_residues = list(residues_for_each_chain.values())
+    for set1 in sets_of_residues:
+        for set2 in sets_of_residues:
+            missing_residues.extend(list(set1.union(set2) - set1.intersection(set2)))
+    print(missing_residues)
+    return missing_residues
+
+def backbone(protein_name: str, chain: chr, residues_to_ignore=[]):
     # Uses a PDB file to get the coordinates of atoms in the backbone of a protein chain
-    backbone_atoms = ['CA'] if only_alpha_carbons else ['N', 'CA', 'C']
     with open(f'{protein_name.lower()}.pdb') as pdb_file:
         pdb_data = pdb_file.read()
     atoms = []
     for line in pdb_data.split('\n'):
-        if line[0:4] == 'ATOM' and line[21] == chain and line[13:16].rstrip(' ') in backbone_atoms:
+        if line[0:4] == 'ATOM' and line[21] == chain and line[13:16].rstrip(' ') == 'CA':
+            residue_num = int(line[24:26])
+            if residue_num in residues_to_ignore:
+                continue
             x = float(line[31:38].rstrip(' '))
             y = float(line[39:46].rstrip(' '))
             z = float(line[47:54].rstrip(' '))
             atoms.append([x, y, z])
-    # Throughout this file, groups of points will be stored mostly as matrices with
-    # each row representing a point, and each column representing a dimension (x, y, or z)
     return np.matrix(atoms)
 
-def dump_beads(*components: np.matrix[float], file_name='beads.txt') -> None:
+def dump_beads(*components, file_name='knotplot/beads.txt') -> None:
     # Given a list of atom coordinates, creates a file for knotplot to open
     knotplot_data = ''
+    components = [component.tolist() for component in components]
     for component_num in range(len(components)):
         knotplot_data += f'Component {component_num + 1} of {len(components)}:\n'
         component = components[component_num]
@@ -32,7 +55,7 @@ def dump_beads(*components: np.matrix[float], file_name='beads.txt') -> None:
     with open(file_name, 'w') as knotplot_file:
         knotplot_file.write(knotplot_data)
 
-def rmsd(chain1: np.matrix[float], chain2: np.matrix[float]):
+def rmsd(chain1, chain2):
     # TODO: create functions for just getting the centroid or rotation matrix
     n = len(chain1)
     if n != len(chain2): raise Error
@@ -52,10 +75,10 @@ def rmsd(chain1: np.matrix[float], chain2: np.matrix[float]):
     total_square_diff = sum([row * row.T for row in chain1 - chain2])[0, 0]
     return math.sqrt(total_square_diff / n)
 
-def angle(points: np.matrix[float]):
+def angle(points):
     # Parameter: list of 3 points, which are each lists of 3 coords
     # returns the angle that they form (in radians)
     points = [np.array(point) for point in points]
     v1 = points[0] - points[1]
     v2 = points[2] - points[1]
-    return math.acos(v1.dot(v2) / np.linalg.norm(v1) / np.linalg.norm(v2))
+    return math.acos(v1.dot(v2.T) / np.linalg.norm(v1) / np.linalg.norm(v2))
