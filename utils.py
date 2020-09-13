41523d22c50c429b21d55b2bb6103d0593bed3e6
Nathan Solomon <nathansolomon1678@gmail.com>
2020-09-12 20:14:34 -0700

    :)

--- utils.py
+++ utils.py
@@ -8,26 +8,7 @@ import math
 import numpy as np
 from typing import *
 
-def missing_residues(protein_name: str, *chains: chr) -> List[int]:
-    # Returns the indeces of residues that are in some chains but not all
-    with open(f'{protein_name.lower()}.pdb') as pdb_file:
-        pdb_data = pdb_file.read()
-    lines = [line for line in pdb_data.split('\n') if line[0:4] == 'ATOM' and line[21] in chains]
-    missing_residues = []
-    residues_for_each_chain = {}
-    for line in lines:
-        residue_num = int(line[22:26])
-        chain = line[21]
-        residues_for_each_chain[chain] = residues_for_each_chain.get(chain, set())
-        residues_for_each_chain[chain].add(residue_num)
-    sets_of_residues = list(residues_for_each_chain.values())
-    for set1 in sets_of_residues:
-        for set2 in sets_of_residues:
-            missing_residues.extend(list(set1.union(set2) - set1.intersection(set2)))
-    print(missing_residues)
-    return missing_residues
-
-def backbone(protein_name: str, chain: chr, residues_to_ignore=[]):
+def backbone(protein_name: str, chain: chr):
     # Uses a PDB file to get the coordinates of atoms in the backbone of a protein chain
     with open(f'{protein_name.lower()}.pdb') as pdb_file:
         pdb_data = pdb_file.read()
@@ -35,16 +16,25 @@ def backbone(protein_name: str, chain: chr, residues_to_ignore=[]):
     for line in pdb_data.split('\n'):
         if line[0:4] == 'ATOM' and line[21] == chain and line[13:16].rstrip(' ') == 'CA':
             residue_num = int(line[24:26])
-            if residue_num in residues_to_ignore:
-                continue
             x = float(line[31:38].rstrip(' '))
             y = float(line[39:46].rstrip(' '))
             z = float(line[47:54].rstrip(' '))
             atoms.append([x, y, z])
     return np.matrix(atoms)
 
-def dump_beads(*components, file_name='knotplot/beads.txt') -> None:
+def dump_beads(*components, file_name='/home/computer/knotplot/beads.txt') -> None:
     # Given a list of atom coordinates, creates a file for knotplot to open
+    # Find centroid of all chains, in order to center it
+    centroid = np.array([[0., 0., 0.]])
+    total_atoms = 0
+    for component in components:
+        for atom in component:
+            centroid += atom[0]
+            total_atoms += 1
+    centroid /= total_atoms
+    for component in components:
+        for atom in component:
+            atom -= centroid
     knotplot_data = ''
     components = [component.tolist() for component in components]
     for component_num in range(len(components)):
@@ -52,7 +42,7 @@ def dump_beads(*components, file_name='knotplot/beads.txt') -> None:
         component = components[component_num]
         for atom in component:
             knotplot_data += ' '.join([str(coord) for coord in atom]) + '\n'
-    with open(file_name, 'w') as knotplot_file:
+    with open(file_name, 'w+') as knotplot_file:
         knotplot_file.write(knotplot_data)
 
 def rmsd(chain1, chain2):
