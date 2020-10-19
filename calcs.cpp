9f55d9c578855ceb7a84ba9d8572b8fe1dbac4ed
Nathan Solomon <nathansolomon1678@gmail.com>
2020-10-19 14:28:33 -0700

    Use multithreading. It's unclear whether this is actually much faster though,
    so I'm probably doing somethig wrong

--- calcs.cpp
+++ calcs.cpp
@@ -8,6 +8,7 @@
 #include <iostream>
 #include <fstream>
 #include <sstream>
+#include <thread>
 
 std::vector<std::array<double, 3>> points_csv() {
   // returns contents of file points.csv as a list of 3D points
@@ -67,31 +68,67 @@ inline double sign(double num) {
   else { return 0.; }
 }
 
+inline double expected_crossing_value(std::array<double, 3> p1,
+                                      std::array<double, 3> p2,
+                                      std::array<double, 3> p3,
+                                      std::array<double, 3> p4) {
+  // Returns the chance that line segment p1 -> p2 will cross p3 -> p4
+  // when viewed from a random 2D projection. If the crossing is negative,
+  // returns the same thing but negative
+  std::array<double, 3> n1 = unit(cross(p3 - p1, p4 - p1));
+  std::array<double, 3> n2 = unit(cross(p4 - p1, p4 - p2));
+  std::array<double, 3> n3 = unit(cross(p4 - p2, p3 - p2));
+  std::array<double, 3> n4 = unit(cross(p3 - p2, p3 - p1));
+  double omega_star = asin(dot(n1, n2)) +
+                      asin(dot(n2, n3)) +
+                      asin(dot(n3, n4)) +
+                      asin(dot(n4, n1));
+  omega_star /= 2 * PI;
+  double omega_signed = omega_star * sign(dot(p3 - p1, cross(p4 - p3, p2 - p1)));
+  return omega_signed;
+}
+
+void add_crossing_values(int i,
+                         std::vector<std::array<double, 3>>* knot,
+                         std::vector<double>* acn_subcalcs,
+                         std::vector<double>* space_writhe_subcalcs) {
+  for (int j = 1; j < i - 1; j++) {
+    std::array<double, 3> p1 = knot->at(i - 1);
+    std::array<double, 3> p2 = knot->at(i);
+    std::array<double, 3> p3 = knot->at(j - 1);
+    std::array<double, 3> p4 = knot->at(j);
+    double crossing_value = expected_crossing_value(p1, p2, p3, p4);
+    space_writhe_subcalcs->at(i) = crossing_value;
+    acn_subcalcs->at(i) += fabs(crossing_value);
+  }
+}
+
+inline double vec_sum(std::vector<double> vec) {
+  double sum = 0;
+  for (double num: vec) {
+    sum += num;
+  }
+  return sum;
+}
+
 std::tuple<double, double> acn_and_writhe(std::vector<std::array<double, 3>> knot) {
   // given a knot as and array of points, returns a tuple (ACN, space writhe)
-  // TODO(Nathan): use multithreading to speed this up even further
-  double acn = 0.;
-  double space_writhe = 0.;
+  // Since the calculation is a bunch of smaller things to be added together,
+  // it splits that up into knot.size() different groups of calcs (subcalcs)
+  // which each have their own thread. They modify values in an array of subcalcs,
+  // which is then added up to create the final calculations
+  std::vector<double> acn_subcalcs(knot.size());
+  std::vector<double> space_writhe_subcalcs(knot.size());
+  std::vector<std::thread> threads = {};
   for (int i = 0; i < knot.size(); i++) {
-    for (int j = 1; j < i - 1; j++) {
-      std::array<double, 3> p1 = knot.at(i - 1);
-      std::array<double, 3> p2 = knot.at(i);
-      std::array<double, 3> p3 = knot.at(j - 1);
-      std::array<double, 3> p4 = knot.at(j);
-      std::array<double, 3> n1 = unit(cross(p3 - p1, p4 - p1));
-      std::array<double, 3> n2 = unit(cross(p4 - p1, p4 - p2));
-      std::array<double, 3> n3 = unit(cross(p4 - p2, p3 - p2));
-      std::array<double, 3> n4 = unit(cross(p3 - p2, p3 - p1));
-      double omega_star = asin(dot(n1, n2)) +
-                          asin(dot(n2, n3)) +
-                          asin(dot(n3, n4)) +
-                          asin(dot(n4, n1));
-      omega_star /= 2 * PI;
-      double omega_signed = omega_star * sign(dot(p3 - p1, cross(p4 - p3, p2 - p1)));
-      acn += omega_star;
-      space_writhe += omega_signed;
-    }
+    std::thread new_thread(add_crossing_values, i, &knot, &acn_subcalcs, &space_writhe_subcalcs);
+    threads.push_back(std::move(new_thread));
   }
+  for (std::thread& current_thread: threads) {
+    current_thread.join();
+  }
+  double acn = vec_sum(acn_subcalcs);
+  double space_writhe = vec_sum(space_writhe_subcalcs);
   return std::make_tuple(acn, space_writhe);
 }
 
